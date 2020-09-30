a5698957c98488ac046888bd01c43979500650f2
Krzysztof Jankowski <k-j@xl.wp.pl>
2020-09-30 14:44:30 +0100

    create Bike class

---
+++ spec/bike_spec.rb
@@ -0,0 +1,11 @@
+require "bike"
+describe Bike do
+  it "creates a new bike" do
+    bike = Bike.new
+    expect(bike).to be_an_instance_of Bike
+  end
+
+  it "can release bike" do
+    expect(Bike.new).to respond_to (:working?)
+  end
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,10 @@ require "docking_station"
 describe DockingStation do
   it "creates a new bike" do
     bike = DockingStation.new
-    expect(bike).to be_truthy
+    expect(bike).to be_an_instance_of DockingStation
+  end
+
+  it "can realease bike" do
+    expect(DockingStation.new).to respond_to (:release_bike)
   end
 end
